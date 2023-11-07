15ffdbb9750dbca94fb2ade6c043c2ac09eb43c5
CarlHenry670 <capic.2023118tads0035@aluno.ifpi.edu.br>
2023-11-07 02:25:31 -0300

    commit teste

--- src/main/java/br/edu/ifpi/App.java
+++ src/main/java/br/edu/ifpi/App.java
@@ -7,5 +7,6 @@ package br.edu.ifpi;
 public class App {
     public static void main(String[] args) {
         System.out.println("Sistema de cursos online!");
+        System.out.println("Versão test");
     }
 }
