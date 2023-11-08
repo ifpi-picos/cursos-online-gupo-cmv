7c0312bd2bfe7f6f6352a85d0f24d3e037d45ddd
MaykonWSF <maykonwillyam038@gmail.com>
2023-11-07 23:40:27 -0300

    Estruturação inicial da classe Aluno

--- src/main/java/br/edu/ifpi/cadastros/Aluno.java
+++ src/main/java/br/edu/ifpi/cadastros/Aluno.java
@@ -1,5 +1,28 @@
 package br.edu.ifpi.cadastros;
 
 public class Aluno {
+    
+    private String nome;
+    private String email;
 
+    public Aluno(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getnome () {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setnome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
